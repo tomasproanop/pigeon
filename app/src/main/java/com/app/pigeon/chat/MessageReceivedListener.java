255ae75a7c134ad77823248a16920e64f4b408af
tomás <67541868+tomasproanop@users.noreply.github.com>
2024-07-02 23:23:31 +0200

    Delete app directory

--- app/src/main/java/com/app/pigeon/chat/MessageReceivedListener.java
+++
@@ -1,5 +0,0 @@
-package com.app.pigeon.chat;
-
-public interface MessageReceivedListener {
-    void onMessageReceived(String message);
-}
--- app/src/main/java/com/app/pigeon/contacts/Contact.java
+++
@@ -1,9 +0,0 @@
-package com.app.pigeon.contacts;
-public interface Contact {
-    int getId();
-    String getName();
-    String getAddress();
-
-    void setName(String string);
-}
-
